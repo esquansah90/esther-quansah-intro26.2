d42035ea7eb3ba26df2a7ec8d125379848a47504
Esther Quansah <263638456+esquansah90@users.noreply.github.com>
2026-05-20 10:13:14 -0400

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
+//Footer #########################################################
 const body = document.querySelector("body");
 const footer = document.createElement("footer");
-body.append(footer);
+body.appendChild(footer);
 
 const today = new Date();
 const thisYear = today.getFullYear();
@@ -9,6 +10,7 @@ const copyright = document.createElement("p");
 copyright.innerHTML = `&copy; Esther Quansah ${thisYear} `;
 footerElement.appendChild(copyright);
 
+//Skills List #########################################################
 const skills = ["HTML", "CSS", "JavaScript", "GitHub", "Excel", "PowerPoint", "Word"];
 
 const skillsSection = document.querySelector("#Skills");
@@ -19,3 +21,48 @@ for (let i = 0; i < skills.length; i++) {
     skill.innerText = skills[i];
     skillsList.appendChild(skill);
 }
+
+//Message Form #########################################################
+
+const messageForm = document.forms.leave_message;
+messageForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+        const usersName = event.target.usersName.value;
+        const usersEmail = event.target.usersEmail.value;
+        const usersMessage = event.target.usersMessage.value;
+    console.log(usersName, usersEmail, usersMessage);
+    const messageSection = document.querySelector("#messages");
+    const messageList = messageSection.querySelector("#messagesList");
+    const newMessage = document.createElement("li");
+    newMessage.innerHTML = `<a href="mailto:${usersEmail}">${usersName}</a> 
+        <span>(${usersEmail})</span> says: 
+        <span>${usersMessage}</span>`;
+    const removeButton = document.createElement("button");
+    removeButton.innerText = "Remove";
+    removeButton.type = "button";
+    removeButton.addEventListener("click", function () {
+        const entry = removeButton.parentNode;
+        entry.remove();
+       
+    });
+
+    const editButton = document.createElement("button");
+
+        editButton.innerText = "Edit";
+        editButton.type = "button";
+
+    editButton.addEventListener("click", function () {
+            const messageContent = newMessage.querySelectorAll("span")[1];
+            const currentText = messageContent.innerText;
+            const newText = prompt ("Edit your message:", currentText);
+            if (newText !== null && newText.trim() !== "") {
+                messageContent.innerText = newText;
+            }
+        }
+    );
+        
+
+    newMessage.appendChild(removeButton);
+    newMessage.appendChild(editButton);
+    messageList.appendChild(newMessage);
+    messageForm.reset(); });
